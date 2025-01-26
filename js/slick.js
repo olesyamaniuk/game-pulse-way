40802403de8b1ce43e9f3768ac80148def775557
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-26 15:10:10 +0200

    3

--- js/slick.js
+++ js/slick.js
@@ -35,23 +35,6 @@ $(function () {
     
   });
 });
-$(function () {
-  $('.testimonials-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: false,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.packages__list-left',
-    nextArrow: '.testimonials-right',
-    
-  });
-});
 
 
 
